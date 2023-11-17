de40b9c1cc994ed48083dc277eaa07961fe3c8f0
ando0pua <parkerhht@ouutlook.jp>
2023-11-17 14:36:19 +0900

    ss

--- MyGitProject2c/src/main/java/controller/gitItemListController.java
+++ MyGitProject2c/src/main/java/controller/gitItemListController.java
@@ -3,10 +3,9 @@ package controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-public class gitItemListController {
-	@RequestMapping(path = "/gititemlist", method = RequestMethod.GET)
-	public String eidht() {
-		return"gititemlist";
+@Controller
+public class gitItemListController{
+	@RequestMapping(path = "/",method = RequestMethod.GET)
 
 	}
 
