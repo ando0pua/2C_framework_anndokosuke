280533f63a30dbd583cc65698c8a400e04e668b5
ando0pua <parkerhht@ouutlook.jp>
2023-11-17 15:00:26 +0900

    aaa

--- MyGitProject2c/src/main/java/controller/ConflictController.java
+++ MyGitProject2c/src/main/java/controller/ConflictController.java
@@ -1,8 +1,17 @@
 package controller;
 
-public class ConflictController 
+public class ConflictController {
+<<<<<<< HEAD
+	//1
+	//2
+	//3
+=======
 
+	//4
+	//5
+	//6
 	//7
 	//8
 	//9
+>>>>>>> refs/remotes/origin/master
 }
