31a5264d795cce8730e540fd827ae75b11a02ac0
S0536 <S0536@19S0536>
2023-11-17 14:34:11 +0900

    aaaaa

---
+++ MyGitProject2c/src/main/java/controller/gitLoginController.java
@@ -0,0 +1,32 @@
+// GitLoginController.java
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class gitLoginController {
+
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("gitlogin");
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String id, @RequestParam String pw) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        // ユーザ認証などの実際の処理はここに実装する
+        if ("gitlogin".equals(id) && "gitpw".equals(pw)) {
+            modelAndView.addObject("message", "ログイン成功！");
+        } else {
+            modelAndView.addObject("message", "ログイン失敗！");
+        }
+
+        modelAndView.setViewName("result");
+        return modelAndView;
+    }
+}
